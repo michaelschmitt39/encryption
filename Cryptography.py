7a7b1dad3b37646935af3c2adf2573d40a2a626a
michaelschmitt39 <57297423+michaelschmitt39@users.noreply.github.com>
2019-11-02 19:40:40 -0500

    Update Cryptography.py

--- Cryptography.py
+++ Cryptography.py
@@ -65,13 +65,13 @@ class VigenereCipher:
         return("".join(cipher_text))
         
     def decrypt(self):
-        plain_text =[]
+        decrypt_text =[]
         cipher_text = look_up_list_gen(plain_text)
         for i in range(len(cipher_text)):
             x = (ord(cipher_text[i])- ord(self.key[i]) + 26) % 26
             x += ord('A')
             plain_text.append(chr(x))
-        return("".join(plain_text))
+        return("".join(decrypt_text))
                    
             
 if __name__ == "__main__":
